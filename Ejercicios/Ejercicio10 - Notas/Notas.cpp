3e13dd172816061784ffc880aae273fd9dffb6d2
pdiacs10 <pdiacs10@gmail.com>
2022-02-07 01:43:49 -0600

    Agregando ejercicio - Notas

--- Ejercicios/Ejercicio10 - Notas/Notas.cpp
+++ Ejercicios/Ejercicio10 - Notas/Notas.cpp
@@ -9,29 +9,31 @@ int main(int argc, char const *argv[])
     cout << "ingrese la nota que obtuvo: ";
     cin >> nota;
 
-    if(nota > 100 || nota < 0)
-    {
-        cout << "Ingrese una nota entre 0 - 100";
-        return 0;
-    }
-
     if(nota >= 95 && nota <= 100) {
         cout << "Obtuviste una A";
+    } else {
+        if(nota >= 85 && nota <= 94) {
+            cout << "Obtuviste una B";
+        }  else {
+            if(nota >= 75 && nota <= 84) {
+                cout << "Obtuviste una C";
+            } else {
+                if(nota >= 65 && nota <= 74) {
+                    cout << "Obtuviste una D";
+                } else {
+                     if(nota >= 60 && nota <= 64) {
+                        cout << "Obtuviste una E";
+                    } else {
+                        if(nota < 60) {
+                            cout <<"Obtuviste una F";
+                        } else {
+                            cout << " Ingrese una notas entre 0- 100";
+                        }
+                    }
+                } 
+            }
+        }      
     }
-    if(nota >= 85 && nota <= 94) {
-        cout << "Obtuviste una B";
-    }
-    if(nota >= 75 && nota <= 84) {
-        cout << "Obtuviste una C";
-    }
-   if(nota >= 65 && nota <= 74) {
-        cout << "Obtuviste una D";
-    }
-    if(nota >= 60 && nota <= 64) {
-        cout << "Obtuviste una E";
-    }
-    if(nota < 60) {
-        cout <<"Obtuviste una F";
-    }
+
     return 0;
 }
