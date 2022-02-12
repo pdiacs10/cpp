af1adb8b75bec439261597cfaa99df206f4b48fb
pdiacs10 <pdiacs10@gmail.com>
2022-02-12 17:25:20 -0600

    Agrengando Ejercicio - ciclo infinito

---
+++ Ejercicios/Ejercicio13 - Ciclos While Infinito/cicloinfinito.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Ejercicio #2
+
+    int ciclos = 0;
+
+    while(ciclos < 1000000)
+    {
+        cout << "*";
+
+        ciclos = ciclos + 1;
+    }
+
+    return 0;
+}
+
+  // Ejercicio #1
+    /*
+    bool esCierto = true;
+
+    if(esCierto) {
+
+    }
+
+    if(true) {
+
+    }
+
+    while(true) {
+        cout << "*";
+    } */
