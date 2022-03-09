54d8249d2969280fa054a45a8cab71cd138419a7
pdiacs10 <pdiacs10@gmail.com>
2022-03-09 02:19:25 -0600

    Agregando Ejercicio - Puntos de Ventas I - V

---
+++ Ejercicios/Ejercicio33 - Punto de vista II/menu.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std; 
+
+extern void productos(int opcion);
+void menu()
+{
+     int opcion = 0;
+
+    while (true)
+    {
+        system("cls");
+
+        cout << "****";
+        cout << "MENU";
+        cout << "****";
+        cout << endl;
+
+        cout << "1 - Bebidas Calientes" << endl;
+        cout << "2 - Bebidas Frias" << endl;
+        cout << "3 - Reposteria" << endl;
+        cout << "0 - Salir" << endl;
+
+        cin >> opcion;
+
+        cout << endl;
+
+        if (opcion == 0)
+        {
+            break;
+        }
+        
+        productos(opcion);
+    }
+    
+}
---
+++ Ejercicios/Ejercicio35 - Punto de vista IV/productos.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std; 
+
+extern void agregarProductos( string descripcion, int cantidad, double precio);
+ 
+void productos(int opcion) 
+{
+    system("cls");
+
+    int opcionPrducto;
+
+    switch (opcion)
+    {
+        case 1:
+        {
+            cout << "BEBIDAS CALIENTES" << endl;
+            cout << "*****************" << endl;
+            cout << "1 - Capuccion" << endl;
+            cout << "2 - Expresso" << endl;
+            cin >> opcionPrducto;
+                
+            switch (opcionPrducto)
+            {
+            case 1:
+                agregarProductos("Capuccion", 1, 40);
+                break;
+             case 2:
+                agregarProductos("Expresso", 1, 30);
+                break;
+                
+            default:
+                {
+                    cout << "opcion no valida";
+                    return;
+                    break;
+                }
+            }
+
+            cout << endl;
+            cout << "Producto agregado" << endl << endl;
+            system("pause");
+        
+            break;
+        }
+        case 2:
+        {
+            cout << "BEBIDAS FRIAS" << endl;
+            cout << "*************" << endl;
+            system("pause");
+            break;
+        }
+        case 3:
+        {
+            cout << "REPOSTERIA" << endl;
+            cout << "**********" << endl;
+            system("pause");
+            break;
+        }
+        default:
+            break;
+        }
+
+}
