0d72564411d4d4c5636e6374f8b564705263dee3
pdiacs10 <pdiacs10@gmail.com>
2022-03-08 18:36:49 -0600

    Agregando Ejercicio - calculadora con funcion

--- Ejercicios/Ejercicio27 - Calculadora con funciones y throw/main.cpp
+++ Ejercicios/Ejercicio27 - Calculadora con funciones y throw/main.cpp
@@ -12,13 +12,14 @@ int calculadora(int a, int b, char operador) {
         return sumar(a, b);
     }
 
-    return -1;
+    throw "El operador no existe";
 }
 
 int main(int argc, char const *argv[])
 {
     int n1 = 0;
     int n2 = 0;
+    int resultado = 0;
     char operador;
 
     cout << "Ingrese el valor de a: ";
@@ -29,8 +30,16 @@ int main(int argc, char const *argv[])
     cin >> operador;
     cout << endl;
 
-    cout << "El resultado de " << n1 << operador << n2 << " es: ";
-    cout << calculadora(n1, n2, operador);
+    try
+    {
+        resultado = calculadora(n1, n2, operador);
+        cout << "El resultado de " << n1 << " " << operador << " " <<  n2 << " es: ";
+        cout << resultado;
+    }
+    catch(const char* error)
+    {
+        cout << error;
+    }
 
     return 0;
 }
