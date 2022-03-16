7bf4561c7a3092ba602f54d94ee1fdd090968b34
pdiacs10 <pdiacs10@gmail.com>
2022-03-15 18:45:55 -0600

    Ejercicio - Random

---
+++ Ejercicios/Ejercicio38 - Random/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero = 0;
+    // inicializa el numero random
+    srand(time(NULL));
+
+    for (int i = 0; i < 20; i++)
+    {
+        // genera un numero entre 1 y 10
+        numero = rand() % 10 + 1;
+        cout  << "Primer numero al azar: " << numero << endl;
+    }
+    
+    return 0;
+}
