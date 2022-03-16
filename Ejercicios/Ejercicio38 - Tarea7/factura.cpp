671aa54eba4c2cb9cdf18e753418dc3bcdf37a63
pdiacs10 <pdiacs10@gmail.com>
2022-03-15 18:50:17 -0600

    Ejercicio - Random

---
+++ Ejercicios/Ejercicio38 - Tarea7/factura.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal;
+double impuesto;
+double total;
+string listaProductos;
+
+void agregarProductos( string descripcion, int cantidad, double precio)
+{
+    listaProductos = listaProductos + descripcion + '\n'; 
+    subtotal = subtotal + (cantidad * precio);
+    impuesto = subtotal * 0.15;
+    total = subtotal + impuesto;
+}
+
+void imprimirFactura() 
+{
+    system("cls");
+    cout << "*******" << endl;
+    cout << "FACTURA" << endl;
+    cout << "*******" << endl;
+    cout << endl;
+
+    cout << "Producto:" << endl;
+    cout << listaProductos;
+
+    cout << endl;
+    cout << "Subtotal: " << subtotal;
+    cout << endl;
+    cout << "Impuesto a pagar (15%): " << impuesto;
+    cout << endl;
+    cout << "Total: " << total;
+    cout << endl;
+    system("pause");
+}
