9e69200b101263c77a2d881c5647ffbfc34cf60b
pdiacs10 <pdiacs10@gmail.com>
2022-03-22 00:49:39 -0600

    Agregando Ejercicio - Arreglos unidimensionales

---
+++ Ejercicios/Ejercicio44 - Arreglos Unidimensionales/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero[5]; // declaracion del arreglo
+
+    // asignacion de valares a un arreglo unidimensional
+    numero[0] = 10;
+    numero[1] = 5;
+    numero[2] = 7;
+    numero[3] = 50;
+    numero[4] = 2;
+
+    // lectura de los valores de un arreglo unidimensional
+    cout << numero[0] << endl;
+    cout << numero[1] << endl;
+    cout << numero[2] << endl;
+    cout << numero[3] << endl;
+    cout << numero[4] << endl;
+
+    return 0;
+}
+    
