e1baf1e677b77d4ac8d07c9ce4e8b0d2eb3f9010
pdiacs10 <pdiacs10@gmail.com>
2022-03-23 15:41:10 -0600

    agregando Ejericio - calcular la raiz cuadrado de un numero

---
+++ Ejercicios/Ejercicio44 - Tarea8/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double R = 0;
+    double raizCuadrado = 0;
+    int numero = 0;
+
+    cout << "Ingrese un numero de Raiz Cuadrado es: ";
+    cin >> numero;
+
+    raizCuadrado = sqrt(numero);
+
+    cout << "La Raiz Cuadrado es: " << raizCuadrado; 
+    return 0;
+}
